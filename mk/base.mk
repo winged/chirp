fc852410235ba13d40cb3ace618cc03efd290bbe
Jean-Louis Fuchs <ganwell@fangorn.ch>
2017-02-15 00:13:03 +0100

    Print checks

--- mk/base.mk
+++ mk/base.mk
@@ -1,10 +1,10 @@
 all: objects
 
 testlibuv:
-	@gcc -c -o "$(DTMP)/uv.o" "$(BASE)/mk/testlibuv.c" >> config.log 2>> config.log
+	@gcc -c -o "$(DTMP)/uv.o" $(CFLAGS) "$(BASE)/mk/testlibuv.c" >> config.log 2>> config.log
 
 testopenssl:
-	@gcc -c -o "$(DTMP)/uv.o" "$(BASE)/mk/testopenssl.c" >> config.log 2>> config.log
+	@gcc -c -o "$(DTMP)/uv.o" $(CFLAGS) "$(BASE)/mk/testopenssl.c" >> config.log 2>> config.log
 
 clean:
 	rm -rf "$(DTMP)"/*
