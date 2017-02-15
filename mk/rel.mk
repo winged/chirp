b1a1ebf2b60c049f7a9b8f1f6290ce7374fd9093
Jean-Louis Fuchs <ganwell@fangorn.ch>
2017-02-16 00:32:52 +0100

    Install headers

--- mk/rel.mk
+++ mk/rel.mk
@@ -33,6 +33,10 @@ endif
 	mkdir -p $(PREFIX)/usr/lib
 	cp -f $(BUILD)/libchirp.a $(PREFIX)/usr/lib
 	cp -f $(BUILD)/libchirp.so $(PREFIX)/usr/lib/libchirp.so.$(VERSION)
+	mkdir -p $(PREFIX)/usr/include
+	cp -f $(BASE)/include/libchirp.h $(PREFIX)/usr/include/libchirp.h
+	rm -rf $(PREFIX)/usr/include/libchirp/
+	cp -rf $(BASE)/include/libchirp/ $(PREFIX)/usr/include/libchirp/
 	cd $(PREFIX)/usr/lib && ln -sf libchirp.so.$(VERSION) libchirp.so
 	cd $(PREFIX)/usr/lib && ln -sf libchirp.so.$(VERSION) libchirp.so.$(MAJOR)
 ifeq ($(DOC),True)
@@ -47,4 +51,6 @@ uninstall:
 	rm -f $(PREFIX)/usr/lib/libchirp.so.$(VERSION)
 	rm -f $(PREFIX)/usr/lib/libchirp.so
 	rm -f $(PREFIX)/usr/lib/libchirp.so.$(MAJOR)
+	rm -f $(PREFIX)/usr/include/libchirp.h
+	rm -rf $(PREFIX)/usr/include/libchirp/
 	rm -rf $(PREFIX)/usr/share/doc/chirp
