5ed17d730e0e5e830c98dcc865ef4dfc5da7845b
Sven Osterwalder <sven.osterwalder@gmail.com>
2017-02-21 09:23:21 +0100

    Cleanup and document pool_test and protocol files

--- src/pool_test.h
+++ src/pool_test.h
@@ -2,25 +2,36 @@
 // Pool test Header
 // ================
 //
+// .. todo:: Document purpose
+//
 // .. code-block:: cpp
 
 #ifndef ch_pool_test_h
 #define ch_pool_test_h
 
+// Project includes
+// ================
+//
+// .. code-block:: cpp
+//
 #include "buffer.h"
 
+// .. c:function::
 extern
 void
 test_ch_bf_init(ch_buffer_pool_t* pool, uint8_t max_buffers);
 
+// .. c:function::
 extern
 void
 test_ch_bf_free(ch_buffer_pool_t* pool);
 
+// .. c:function::
 extern
 ch_bf_handler_t*
 test_ch_bf_reserve(ch_buffer_pool_t* pool);
 
+// .. c:function::
 extern
 void
 test_ch_bf_return(ch_buffer_pool_t* pool, ch_bf_handler_t* handler_buf);
--- src/protocol.h
+++ src/protocol.h
@@ -2,25 +2,76 @@
 // Protocol Header
 // ===============
 //
+// .. todo:: Document purpose
+//
 // .. code-block:: cpp
 
 #ifndef ch_protocol_h
 #define ch_protocol_h
 
+// Project includes
+// ================
+//
+// .. code-block:: cpp
+//
 #include "libchirp/chirp.h"
 #include "connection.h"
 #include "sglib.h"
 
+// Sglib Prototypes
+// ================
+
+// .. c:macro:: CH_RECEIPT_CMP
+//
+//    Compares two receipts memorywise as byte strings, assuming that both byte
+//    strings are 16 bytes long.
+//
+//    :param x: First red-black tree node to get receipt (byte string) from.
+//    :param y: Second red-black tree node to get receipt (byte string) from.
+//
+// .. code-block:: cpp
+//
+#define CH_RECEIPT_CMP(x,y) \
+    memcmp(x->receipt, y->receipt, 16)
+
+// .. code-block:: cpp
+//
+SGLIB_DEFINE_RBTREE_PROTOTYPES( // NOCOV
+    ch_receipt_t,
+    left,
+    right,
+    color_field,
+    CH_RECEIPT_CMP
+)
+
+// Declarations
+// ============
+
 // .. c:type:: ch_receipt_t
 //
-//    Receipt set implemented as rbtree.
+//    Receipt set implemented as red-black tree.
 //
 //    .. c:member:: unsigned char receipt[16]
 //
-//    the receipt generated using TODO
+//       Identity of the receipt, represented as 16 byte long byte string.
+//
+//    .. c:member:: char color_field
+//
+//       The color of the current (protocol-) node. This may either be red or
+//       black, as receipts are built as a red-black tree.
+//
+//    .. c:member:: ch_receipt_s* left
+//
+//       (Struct-) Pointer to the left child of the current receipt (node)
+//       in the red-black tree.
+//
+//    .. c:member:: ch_receipt_s* right
+//
+//       (Struct-) Pointer to the right child of the current receipt (node)
+//       in the red-black tree.
 //
 // .. code-block:: cpp
-
+//
 typedef struct ch_receipt_s {
   unsigned char        receipt[16];
   char                 color_field;
@@ -34,24 +85,49 @@ typedef struct ch_receipt_s {
 //
 //    .. c:member:: struct sockaddr_in addrv4
 //
-//       BIND_V4 addr converted to a sockaddr_in
+//       BIND_V4 address converted to a sockaddr_in.
 //
-//    .. c:member:: struct sockaddr_in addrv4
+//    .. c:member:: struct sockaddr_in addrv6
 //
-//       BIND_V6 addr converted to a sockaddr_in6
+//       BIND_V6 address converted to a sockaddr_in6.
 //
 //    .. c:member:: uv_tcp_t serverv4
 //
-//       reference to the libuv tcp server
+//       Reference to the libuv tcp server handle, IPv4.
 //
 //    .. c:member:: uv_tcp_t serverv6
 //
-//       reference to the libuv tcp server
+//       Reference to the libuv tcp server handle, IPv6.
 //
-//       TODO Complete
+//    .. c:member:: ch_connection_t* connections
+//
+//       Pointer to connections that are used for this protocol.
+//
+//    .. c:member:: ch_connection_t* old_connections
+//
+//       Pointer to old connections. This is mainly used when there is a
+//       network race condition. The then current connections will be replaced
+//       and saved as old connections for garbage collection.
+//
+//    .. c:member:: ch_receipt_t* receipts
+//
+//       .. todo:: Check if the following is correct.
+//
+//       Pointer to receipts for this protocol.
+//
+//    .. c:member:: ch_receipt_t* late_receipts
+//
+//       .. todo:: Check if the following is correct.
+//
+//       Pointer to a set of receipts for this protocol that experience (too)
+//       late delivery.
+//
+//    .. c:member:: ch_chirp_t* chirp
+//
+//       Pointer to the chirp object. See: :c:type:`ch_chirp_t`.
 //
 // .. code-block:: cpp
-
+//
 typedef struct ch_protocol_s {
     struct sockaddr_in  addrv4;
     struct sockaddr_in6 addrv6;
@@ -64,17 +140,30 @@ typedef struct ch_protocol_s {
     ch_chirp_t*         chirp;
 } ch_protocol_t;
 
+// .. c:function::
+ch_error_t
+ch_pr_start(ch_protocol_t* protocol);
+//
+//    Start the given protocol.
+//
+//    :param ch_protocol_t* protocol: Protocol which shall be started..
+//
+//    :return: A chirp error. see: :c:type:`ch_error_t`
+//    :rtype:  ch_error_t
 
-#define CH_RECEIPT_CMP(x,y) \
-    memcmp(x->receipt, y->receipt, 16)
+// .. c:function::
+ch_error_t
+ch_pr_stop(ch_protocol_t* protocol);
+//
+//    Stop the given protocol.
+//
+//    :param ch_protocol_t* protocol: Protocol which shall be stopped.
+//
+//    :return: A chirp error. see: :c:type:`ch_error_t`
+//    :rtype:  ch_error_t
 
-SGLIB_DEFINE_RBTREE_PROTOTYPES(
-    ch_receipt_t,
-    left,
-    right,
-    color_field,
-    CH_RECEIPT_CMP
-)
+// Definitions
+// ===========
 
 // .. c:function::
 static
@@ -82,10 +171,10 @@ ch_inline
 void
 ch_pr_init(ch_chirp_t* chirp, ch_protocol_t* protocol)
 //
-//    Initialize the protocol struct.
+//    Initialize the protocol structure.
 //
-//    :param ch_chirp_t* chirp: Chirp instance
-//    :param ch_protocol_t* protocol: Protocol to initialize
+//    :param ch_chirp_t* chirp: Chirp instance.
+//    :param ch_protocol_t* protocol: Protocol to initialize.
 //
 // .. code-block:: cpp
 //
@@ -94,23 +183,6 @@ ch_pr_init(ch_chirp_t* chirp, ch_protocol_t* protocol)
     protocol->chirp = chirp;
 }
 
-// .. c:function::
-ch_error_t
-ch_pr_start(ch_protocol_t* protocol);
-//
-//    Start the protocol
-//
-//    TODO params
-//
-
-// .. c:function::
-ch_error_t
-ch_pr_stop(ch_protocol_t* protocol);
-//
-//    Stop the protocol
-//
-//    TODO params
-//
 // .. code-block:: cpp
-
+//
 #endif //ch_protocol_h
