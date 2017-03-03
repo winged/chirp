ff43cbfedf4079e317fa64e420a22cbe0636f093
Jean-Louis Fuchs <ganwell@fangorn.ch>
2017-03-03 13:18:58 +0100

    etests should depend on all

--- mk/dev.mk
+++ mk/dev.mk
@@ -41,7 +41,7 @@ endif
 help:  ## Display this help
 	@cat $(MAKEFILE_LIST) | grep -E '^[0-9a-zA-Z_-]+:.*?## .*$$' | sort -k1,1 | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-etests:
+etests: all
 	$(BUILD)/src/chirp_etest & \
 	PID=$$!; \
 	sleep 1; \
