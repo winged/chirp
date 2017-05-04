2bfd52abe691b9bad1028cd8926fb63c9f217b4b
Jean-Louis Fuchs <ganwell@fangorn.ch>
2017-05-04 15:32:12 +0200

    Move install/uninstall to rules.mk so dev can install too

--- mk/rules.mk
+++ mk/rules.mk
@@ -145,3 +145,33 @@ endif
 ifeq ($(DEV),True)
 	@command -v setfattr > /dev/null && setfattr -n user.pax.flags -v "emr" $@
 endif
+
+ifeq ($(DOC),True)
+install: all doc
+else
+install: all  ## Install chirp
+endif
+	mkdir -p $(PREFIX)/usr/lib
+	cp -f $(BUILD)/libchirp.a $(PREFIX)/usr/lib
+	cp -f $(BUILD)/libchirp.so $(PREFIX)/usr/lib/libchirp.so.$(VERSION)
+	mkdir -p $(PREFIX)/usr/include
+	cp -f $(BASE)/include/libchirp.h $(PREFIX)/usr/include/libchirp.h
+	rm -rf $(PREFIX)/usr/include/libchirp/
+	cp -rf $(BASE)/include/libchirp/ $(PREFIX)/usr/include/libchirp/
+	cd $(PREFIX)/usr/lib && ln -sf libchirp.so.$(VERSION) libchirp.so
+	cd $(PREFIX)/usr/lib && ln -sf libchirp.so.$(VERSION) libchirp.so.$(MAJOR)
+ifeq ($(DOC),True)
+	rm -rf $(PREFIX)/usr/share/doc/chirp
+	mkdir -p $(PREFIX)/usr/share/doc/chirp
+	cp -R $(BASE)/doc/_build/html/* $(PREFIX)/usr/share/doc/chirp/
+endif
+
+
+uninstall:  ## Uninstall chirp
+	rm -f $(PREFIX)/usr/lib/libchirp.a
+	rm -f $(PREFIX)/usr/lib/libchirp.so.$(VERSION)
+	rm -f $(PREFIX)/usr/lib/libchirp.so
+	rm -f $(PREFIX)/usr/lib/libchirp.so.$(MAJOR)
+	rm -f $(PREFIX)/usr/include/libchirp.h
+	rm -rf $(PREFIX)/usr/include/libchirp/
+	rm -rf $(PREFIX)/usr/share/doc/chirp
