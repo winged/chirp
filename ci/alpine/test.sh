816f24099466ac5c6e3f049c71177f0d9a723865
Jean-Louis Fuchs <ganwell@fangorn.ch>
2017-09-27 00:29:26 +0200

    Also test install, uninstall and clean

--- ci/alpine/test.sh
+++ ci/alpine/test.sh
@@ -10,4 +10,10 @@ if [ "$TESTSHELL" = "True" ]; then
 else
     make test
     make doc
+    mkdir -p out
+    make install DEST=out
+    [ -f out/usr/local/lib/libchirp.so ]
+    make uninstall DEST=out
+    [ ! -f out/usr/local/lib/libchirp.so ]
+    make clean
 fi
